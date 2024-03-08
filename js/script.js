26fcb2e3b6de8f265735cef4ed4b6b423db94ade
helloprof <suluxan.seneca@gmail.com>
2024-03-08 18:56:28 -0500

    new script

---
+++ js/script.js
@@ -0,0 +1,45 @@
+/**
+ * WEB222 – Assignment 04
+ *
+ * I declare that this assignment is my own work in accordance with
+ * Seneca Academic Policy. No part of this assignment has been
+ * copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Please update the following with your information:
+ *
+ *      Name:       <YOUR_NAME>
+ *      Student ID: <YOUR_STUDENT_ID>
+ *      Date:       <SUBMISSION_DATE>
+ */
+
+// All of our data is available on the global `window` object.
+// Create local variables to work with it in this file.
+const { artists, songs } = window;
+
+// For debugging, display all of our data in the console. You can remove this later.
+console.log({ artists, songs }, "App Data");
+
+function generateArtistButtons(artists) {
+    console.log("generating artists")
+
+    const menu = document.querySelector('#menu')
+    artists.forEach(function(artist) {
+        const artistButton = document.createElement('button')
+        artistButton.textContent = artist.name
+        artistButton.addEventListener("click", (e) => {
+            console.log("clicked!")
+        })
+
+        menu.appendChild(artistButton)
+    })
+}
+
+
+function onLoad() {
+    console.log("page loaded")
+    // main function here 
+    generateArtistButtons(artists)
+}
+
+window.addEventListener("load", onLoad)
